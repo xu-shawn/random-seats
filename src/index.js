d6d08e1394e7cc421e8c89d40204a88e81e30ac6
OwO <OwO@Fire>
2022-10-27 14:57:45 -0700

    Update the Webpack and add WebGL detection support

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 import * as THREE from 'three';
 
 import { Scene } from 'three';
+import {element} from "three/addons/nodes/shadernode/ShaderNodeBaseElements";
 
-const scene = new Scene();
+function isWebGLAvailable() {
+    try {
+        const canvas = document.createElement( 'canvas' );
+        return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) );
+    } catch ( e ) {
+        return false;
+    }
+}
 
-if (!WebGL.isWebGLAvailable()) {
+if (isWebGLAvailable()) {
+    init();
+    // animate();
+} else {
+    document.getElementById('container').innerHTML = 'Your browser or graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>';
+}
 
-    const warning = WebGL.getWebGLErrorMessage();
-    document.getElementById( 'container' ).appendChild( warning );
+function init() {
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
 
+    const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
+    camera.position.set( 0, 0, 100 );
+    camera.lookAt( 0, 0, 0 );
+
+    const scene = new THREE.Scene();
+
+    //create a blue LineBasicMaterial
+    const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+    const points = [];
+    points.push( new THREE.Vector3( - 10, 0, 0 ) );
+    points.push( new THREE.Vector3( 0, 10, 0 ) );
+    points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    const line = new THREE.Line( geometry, material );
+
+    scene.add( line );
+    renderer.render( scene, camera );
+
+}
+function animate() {
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
-var path = require('path');
+const path = require('path');
+
 module.exports = {
-  mode: 'development'
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js',
+  }
 };
